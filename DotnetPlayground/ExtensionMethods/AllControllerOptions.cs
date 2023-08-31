b381e08e6059545cbd406e5a13b278805eb7d0e1
sreejithout <71999724+sreejithout@users.noreply.github.com>
2023-08-31 10:43:50 +0530

    Update AllControllerOptions.cs

--- DotnetPlayground/ExtensionMethods/AllControllerOptions.cs
+++ DotnetPlayground/ExtensionMethods/AllControllerOptions.cs
@@ -9,13 +9,21 @@ public static class AllControllerOptions
 {
     public static void RegisterControllerOptions(this MvcOptions options)
     {
+        // Adding a route convention
         var parameterTransformer = new SlugifyParameterTransformer();
         var routeTokenTransformerConvention = new RouteTokenTransformerConvention(parameterTransformer);
         options.Conventions.Add(routeTokenTransformerConvention);
 
+        // Adding Global Action filter
         options.Filters.Add(new SampleGlobalActionFilter());
+
+        // Adding Global Resource filter with attribute
         options.Filters.Add(new SampleResourceFilterAttribute("Global"));
+
+        // Adding Global Result filter for supporting dependency injection
         options.Filters.AddService<SampleResultFilterAttribute>();
+
+        // Adding Global Result filter for supporting dependency injection
         options.Filters.Add<SampleResultFilterAttribute>();
     }
 }
