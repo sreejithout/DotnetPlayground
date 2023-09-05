94b8475d4e1ffd46858587e786b4a381577867e2
sreejithout <sreejithrknss@outlook.com>
2023-09-05 15:43:11 +0530

    Authentication using Identity

--- DotnetPlayground/Utilities/Interfaces/IJWTGenerator.cs
+++ DotnetPlayground/Utilities/Interfaces/IJWTGenerator.cs
@@ -1,8 +1,10 @@
-﻿using SharedPocos.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using SharedPocos.Models;
 
 namespace DotnetPlayground.WebApi.Utilities.Interfaces;
 
 public interface IJWTGenerator
 {
+    string GenerateIdentityJwt(IdentityUser user);
     string GenerateJwt(TokenGenerationRequest request);
 }
