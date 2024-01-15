d3d87dae1738465a33b2b698c22b8e1782913989
sreejithout <sreejithrknss@outlook.com>
2024-01-15 19:48:06 +0530

    adding services to DI

---
+++ Services/ServiceModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Interfaces;
+
+namespace Services;
+
+public static class ServiceModule
+{
+    /// <summary>
+    /// Extension method to inject Dependencies from this project
+    /// </summary>
+    /// <param name="services"></param>
+    public static void AddServices(this IServiceCollection services)
+    {
+        // Transient: created each time they are requested for.
+        // Scoped: created once in the entire request scope.
+        // Singleton: created once in the application lifetime.
+        services.AddTransient<IProductService, ProductService>();
+        services.AddTransient<ICustomerService, CustomerService>();
+    }
+}
