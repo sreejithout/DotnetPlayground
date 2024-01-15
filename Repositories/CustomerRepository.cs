9f100531b79710e016f0f9d82eed43c8e5429c35
sreejithout <sreejithrknss@outlook.com>
2024-01-15 19:48:47 +0530

    EF Core Change tracking for Eager Loading Search Queries

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -35,10 +35,22 @@ internal class CustomerRepository : ICustomerRepository
         return _dbContext.Customers;
     }
 
+    /// <summary>
+    /// GetCustomer
+    /// 
+    /// Change Tracking:
+    /// When EF Core queries a DB, it stores the snapshot of the result set in memory
+    /// Now, any modifications you made into that entity is actually made to that snapshot and then later written to DB.
+    /// In a read only scenario, there's no chance we are going to write data back to the DB. 
+    /// So we can skip the snapshot in the above readonly scenario.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public async Task<Customer> GetCustomer(int id)
     {
         return await _dbContext.Customers
             .Include(c => c.Orders) // Eager Loading
+            .AsNoTracking() // To Disable change tracking.
             .SingleAsync(x => x.Id == id);
     }
 
